3fcc7cbeea150f786992857861a44227c3e218ae
Pierre Juaneda <pierre.juaneda@gmail.com>
2020-11-03 22:49:44 +0100

    Create home fragment

--- app/src/main/java/com/pjuaneda/shifumi/HomeFragment.kt
+++ app/src/main/java/com/pjuaneda/shifumi/HomeFragment.kt
@@ -6,26 +6,28 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import androidx.core.os.bundleOf
 import androidx.navigation.fragment.findNavController
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
-class FirstFragment : Fragment() {
+class HomeFragment : Fragment() {
 
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false)
+        return inflater.inflate(R.layout.fragment_home, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        view.findViewById<Button>(R.id.button_first).setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+        view.findViewById<Button>(R.id.home_play_human).setOnClickListener {
+            findNavController().navigate(R.id.action_home_to_game)
         }
+
     }
 }
